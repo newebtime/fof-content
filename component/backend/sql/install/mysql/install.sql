03fedbdef425fe4f9cee047d45701cbdfde1c6a8
Eighke <eighke@multi-site.net>
2015-11-05 20:16:25 +0700

    Added creator, editor and related dates

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS `#__fofcontent_contents` (
   `fofcontent_content_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `enabled` TINYINT(1) NOT NULL,
+  `created_by` INT(11) UNSIGNED NOT NULL,
+  `created_on` DATE NOT NULL,
+  `modified_by` INT(11) UNSIGNED NOT NULL,
+  `modified_on` DATE NOT NULL
   PRIMARY KEY ( `fofcontent_content_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
