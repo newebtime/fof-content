f70dbf51258aa2774daf0649a690be435b3673dd
Eighke <eighke@multi-site.net>
2015-11-05 21:12:37 +0700

    Added access, language, slug; enabled tab on form

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -1,10 +1,14 @@
 CREATE TABLE IF NOT EXISTS `#__fofcontent_contents` (
   `fofcontent_content_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
+  `title` varchar(255) NULL,
+  `slug` VARCHAR(255) NOT NULL,
+  `fulltext` TEXT NOT NULL,
   `enabled` TINYINT(1) NOT NULL,
   `created_by` INT(11) UNSIGNED NOT NULL,
   `created_on` DATE NOT NULL,
   `modified_by` INT(11) UNSIGNED NOT NULL,
-  `modified_on` DATE NOT NULL
+  `modified_on` DATE NOT NULL,
+  `access` INT(10) UNSIGNED NOT NULL,
+  `language` CHAR(7) NOT NULL,
   PRIMARY KEY ( `fofcontent_content_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
