85f074790d2d5a95693d148880a176d852b7c0a5
Eighke <eighke@multi-site.net>
2015-11-05 20:05:04 +0700

    Added enabled field

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `#__fofcontent_contents` (
   `fofcontent_content_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `enabled` TINYINT(1) NOT NULL,
   PRIMARY KEY ( `fofcontent_content_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
