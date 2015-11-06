be0c3d17ae4421abb9ab3c0c413902b6bf9de733
Eighke <eighke@multi-site.net>
2015-11-06 11:57:51 +0700

    Added hits and order in list

--- component/backend/sql/install/mysql/install.sql
+++ component/backend/sql/install/mysql/install.sql
@@ -8,7 +8,9 @@ CREATE TABLE IF NOT EXISTS `#__fofcontent_contents` (
   `created_on` DATE NOT NULL,
   `modified_by` INT(11) UNSIGNED NOT NULL,
   `modified_on` DATE NOT NULL,
+  `ordering` INT(11) NOT NULL,
   `access` INT(10) UNSIGNED NOT NULL,
+  `hits` INT(11) UNSIGNED NULL,
   `language` CHAR(7) NOT NULL,
   PRIMARY KEY ( `fofcontent_content_id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
